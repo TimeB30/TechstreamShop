9d6f84b94d0d6b3a1c13cd285dbf9432e8f675e5
TimeB <timairisov@gmail.com>
2026-03-08 16:30:44 +0300

    kafka client fix

--- pkg/kafkaclient/config.go
+++ pkg/kafkaclient/config.go
@@ -2,10 +2,10 @@ package kafkaclient
 
 import "github.com/confluentinc/confluent-kafka-go/v2/kafka"
 
-func getKafkaConfig(config map[string]interface{}) kafka.ConfigMap {
+func getKafkaConfig(config map[string]interface{}) *kafka.ConfigMap {
 	kafkaConfig := kafka.ConfigMap{}
 	for key, value := range config {
 		kafkaConfig[key] = value
 	}
-	return kafkaConfig
+	return &kafkaConfig
 }
